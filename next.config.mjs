2754a3a12275163471dcdf5d8fee1151aaa373d7
josiah-praise <praisechinonso21@gmail.com>
2025-06-03 11:11:45 +0100

    bug fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "randomuser.me",
-        port: "",
-        pathname: "/api/portraits/**",
-        search: "",
-      },
-    ],
+    domains: ["randomuser.me"],
   },
 };
 
