bc3484853b7f2b211a94605bd8194f33945db52e
josiah-praise <praisechinonso21@gmail.com>
2025-06-03 11:01:00 +0100

    modified nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,9 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "randomuser.me",
+        port: "",
+        pathname: "/api/portraits/**",
+        search: "",
       },
     ],
   },
